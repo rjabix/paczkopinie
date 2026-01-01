af593ff712dd1dc658d8f7526bff8f85ffb87347
Paul0081 <pgryszczyk@gmail.com>
2026-01-01 18:36:15 +0100

    Skończone porządkowanie __init__.py, przeniesione mail secrets do .env

--- website/__init__.py
+++ website/__init__.py
@@ -39,20 +39,13 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-
     app.config.update(
-        # konfiguracja SMTP (w testach wstawiamy wartości bezpośrednio)
-        # MOŻE PRZENIEŚĆ DO SECRETS
-        #MAIL user i pw tu bo inaczej bledny mail przychodzi
-        # ???? chyba nie, trzeba uporządkować i przetestować na aws, obecnie dziala
-        SECRET_KEY = os.environ.get('APP_SECRET_KEY'),
-        MAIL_SERVER='smtp.gmail.com',
-        MAIL_PORT=587,
-        MAIL_USE_TLS=True,
+        MAIL_SERVER=os.environ.get('MAIL_SERVER'),
+        MAIL_PORT=os.environ.get('MAIL_PORT'),
+        MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS'),
         MAIL_USERNAME = os.environ.get('MAIL_ACCOUNT'),
         MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD'),
     )
-
     mail.init_app(app)
     migrate.init_app(app, db)
 
