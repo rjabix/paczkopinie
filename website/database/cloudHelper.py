e97aa57e7bd5a5c576cc949356faadba552def86
lonely-wolf7 <lonelywolf.night@gmail.com>
2026-01-06 22:04:40 +0100

    Clean up comments in cloudHelper.py
    Removed unnecessary comments from cloudHelper.py

--- website/database/cloudHelper.py
+++ website/database/cloudHelper.py
@@ -1,6 +1,3 @@
-# Use this code snippet in your app.
-# If you need more information about configurations
-# or implementing the sample code, visit the AWS docs:
 # https://aws.amazon.com/developer/language/python/
 
 import json
